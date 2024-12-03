54aaeaf44ea86fb17437c7dcbf563fd7227eb8c0
fmnavarrete <fmnavarrete@gbox.ncf.edu.ph>
2024-12-03 14:32:47 +0800

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,15 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function About () {
+    return (
+        <ListGroup>
+          <ListGroup.Item>Jerome B. Millanes</ListGroup.Item>
+          <ListGroup.Item>06-01-2004 </ListGroup.Item>
+          <ListGroup.Item>18</ListGroup.Item>
+          <ListGroup.Item>Single</ListGroup.Item>
+          <ListGroup.Item>Student</ListGroup.Item>
+        </ListGroup>
+      );
+    }
+    
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home () {
+    return (
+        <>
+        <h1>tata</h1>
+        </>
+    )
+}
+
+export default Home;
